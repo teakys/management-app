de606196f90d5309635a94b83bc06a32982288bd
matheus <mateakys@gmail.com>
2025-11-08 02:00:02 -0300

    Chore: Set up Eslint 9 with flat config

--- eslint.config.ts
+++ eslint.config.ts
@@ -13,10 +13,16 @@ export default defineConfigWithVueTs(
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
+  },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
   skipFormatting,
+  { rules: { 'vue/multi-word-component-names': 0 } }
 )
+
